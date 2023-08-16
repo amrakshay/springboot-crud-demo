567b35ea2d3223503ba73c163f57d797de1c3d2f
amrakshay <amrakshay@gmail.com>
2023-08-16 17:08:52 +0530

    Update ProductController.java

--- src/main/java/com/ensat/controllers/ProductController.java
+++ src/main/java/com/ensat/controllers/ProductController.java
@@ -4,13 +4,7 @@ import com.ensat.entities.Product;
 import com.ensat.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 
