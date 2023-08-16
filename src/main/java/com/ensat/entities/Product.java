a6bfec23e7923232e52925a718ea3ab0bb0fc634
amrakshay <amrakshay@gmail.com>
2023-08-16 17:09:09 +0530

    Update Product.java

--- src/main/java/com/ensat/entities/Product.java
+++ src/main/java/com/ensat/entities/Product.java
@@ -1,5 +1,9 @@
 package com.ensat.entities;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
